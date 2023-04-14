22f4d2f108b080b83ddd6ea095a75f81681e0c6a
Vairavel-1999 <125169932+Vairavel-1999@users.noreply.github.com>
2023-04-14 18:18:35 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,46 @@
+// var div=document.createElement("div");
+// console.log(div);
+// div.innerHTML="this is div";
+
+// document.body.append(div); 
+
+
+// var pg=document.createElement("p");
+
+// pg.innerHTML="this is pragraph";
+
+// document.body.append(pg); 
+
+// var div1=document.createElement("div");
+// div1.innerHTML="i am the devil";
+// div1.style.backgroundColor="green";
+// document.body.append(div1);
+
+//  var container=document.createElement("div");
+//  container.setAttribute("class","container");
+
+
+//  var row=document.createElement("div");
+//  row.setAttribute("class","row");
+ 
+
+//  var col=document.createElement("div");
+//  col.setAttribute("class","col");
+
+
+//  row.append(col);
+//  container.append(row);
+//  document.body.append(container);
+
+
+function ssk(){
+    var res=document.getElementById("first").value;
+    var res1=document.getElementById("middle").value;
+    var res2=document.getElementById("last").value;
+    var res3=document.getElementById("email").value;
+
+    console.log(res);
+    console.log(res1);
+    console.log(res2);
+    console.log(res3);
+}
